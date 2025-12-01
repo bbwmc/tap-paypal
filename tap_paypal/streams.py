8be2f92d675c4051524b0b6e6b1c892481e73e6c
theissn <theissnielsen@gmail.com>
2025-12-01 15:09:58 +0000

    fix error

--- tap_paypal/streams.py
+++ tap_paypal/streams.py
@@ -5,7 +5,8 @@ from types import MappingProxyType
 # Streams metadata
 STREAMS: MappingProxyType = MappingProxyType({
     'paypal_transactions': {
-        'key_properties': ['transaction_info', 'transaction_id'],
+        # Only include scalar keys so Singer targets can hash primary keys
+        'key_properties': ['transaction_id'],
         'replication_method': 'INCREMENTAL',
         'replication_key': 'transaction_info.transaction_updated_date',
         'bookmark': 'start_date',
